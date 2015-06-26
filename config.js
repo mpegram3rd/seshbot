0d6fc6e65c618be2f20c92a424171d073563ce8c
Macon Pegram <mpegram@captechventures.com>
2015-06-25 22:40:05 -0400

    Added mock behavior for Untappd until we can hit the real deal.

--- config.js
+++ config.js
@@ -1,6 +1,63 @@
-exports.webHookURL = 'http://put.your.callbacklink.here';
+exports.webHookURL = 'https://hooks.slack.com/services/YOUR/MAGICNUMBER';
 exports.usermap = {
     'slackusername1' : 'untappdusername1',
     'slackusername2' : 'untappdusername2'
 };
 
+// Stubbed until Untappd API is available
+exports.beerInfo = new Object();
+exports.beerInfo['UNDEFINED'] =
+    [
+        {
+            beer : 'Coronoa Light',
+            brewery : 'Grupo Modelo S.A. de C.V.'
+        }
+    ];
+
+exports.beerInfo['UNTAPPDID1'] =
+    [
+        {
+            beer : 'Dark Rye',
+            brewery : 'Ardent Craft Ales'
+        },
+        {
+            beer : 'Nuclear Nugget',
+            brewery : 'Lickinghole Creek Craft Brewery'
+        },
+        {
+            beer : "G'Knight",
+            brewery : 'Oskar Blues Brewery'
+        },
+        {
+            beer : 'Bourbon Cru',
+            brewery : 'Hardywood Park Craft Brewery'
+        },
+        {
+            beer : "American Beauty",
+            brewery : 'Dogfish Head Craft Brewery'
+        }
+    ];
+
+exports.beerInfo['UNTAPPDID2'] =
+    [
+        {
+            beer : 'Accumulation',
+            brewery : 'New Belgium Brewing Company'
+        },
+        {
+            beer : 'Hopslam Ale (2014)',
+            brewery : "Bell's Brewery, Inc."
+        },
+        {
+            beer : "Double Trouble",
+            brewery : 'Founders Brewing Co.'
+        },
+        {
+            beer : 'Double IPA',
+            brewery : 'Ardent Craft Ales'
+        },
+        {
+            beer : "Grand Larceny",
+            brewery : 'The Answer'
+        }
+    ];
