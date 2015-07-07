4daae4c01f34748462cb71e8a8ed005346461f9b
Macon Pegram <mpegram@captechventures.com>
2015-07-06 23:22:14 -0400

    removed errant comma

--- slack/config.js
+++ slack/config.js
@@ -1,5 +1,5 @@
 exports.webHookURL = 'https://hooks.slack.com/services/YOUR_INCOMING_WEBHOOK_URL';
 exports.usermap = {
     '@SLACK_USER_NAME1' : 'UNTAPPD_USER_NAME1',
-    '@SLACK_USER_NAME2' : 'UNTAPPD_USER_NAME2',
+    '@SLACK_USER_NAME2' : 'UNTAPPD_USER_NAME2'
 };
